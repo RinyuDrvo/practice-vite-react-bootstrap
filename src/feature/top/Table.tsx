c02176964cc0deef88939faa49ee675f0ea893b1
RinyuDrvo <alchemist.drumvocal@gmail.com>
2023-01-29 22:44:14 +0900

    ソート実装

--- src/feature/top/Table.tsx
+++ src/feature/top/Table.tsx
@@ -1,25 +1,32 @@
 import { Table as BTable } from 'react-bootstrap'
 import React from 'react'
 import { Company } from '@/models/Company'
-import { ColumnDef, flexRender, getCoreRowModel, getPaginationRowModel, useReactTable } from '@tanstack/react-table'
+import { ColumnDef, flexRender, getCoreRowModel, getPaginationRowModel, getSortedRowModel, SortingState, useReactTable } from '@tanstack/react-table'
 
 /** テーブル */
 export const Table = ({ data, columns }: { data: Company[], columns: ColumnDef<Company>[] }) => {
+  const [sorting, setSorting] = React.useState<SortingState>([])
   const table = useReactTable({
     data,
     columns,
+    state: {
+      sorting
+    },
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel()
   })
 
   return (
-    <BTable border={1}>
+    <BTable>
       <thead>
         {table.getHeaderGroups().map(headerGroup => (
           <tr key={headerGroup.id}>
             {headerGroup.headers.map(header => (
-              <th key={header.id}>
-                {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+              <th key={header.id} onClick={header.column.getToggleSortingHandler()}>
+                {flexRender(header.column.columnDef.header, header.getContext())}
+                {{ asc: '↑', desc: '↓' }[header.column.getIsSorted() as string] ?? null}
               </th>
             ))}
           </tr>
