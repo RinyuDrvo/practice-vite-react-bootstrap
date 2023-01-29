ea2958286c52faeb2b183cb1c25066fe15c7d8b4
RinyuDrvo <alchemist.drumvocal@gmail.com>
2023-01-29 16:26:12 +0900

    navbar仮実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { AppRoutes } from "@/routes"
 import { BrowserRouter } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,7 +1,18 @@
+import { Navbar, Container, Nav } from 'react-bootstrap'
+
 /** トップページ */
 export const Top = () => {
   return (
     <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">practice-vite-react-bootstrap</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Top</Nav.Link>
+            <Nav.Link href="/detail">Detail</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
       <h1>top page</h1>
     </>
   )
