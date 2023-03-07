ed5f7c28ed498f37030793302446d1eceab0cc93
RinyuDrvo <alchemist.drumvocal@gmail.com>
2023-03-07 18:54:32 +0900

    ユーザー情報を取得し表示

--- src/components/laytouts/MainLayout.tsx
+++ src/components/laytouts/MainLayout.tsx
@@ -1,3 +1,6 @@
+import { getUser } from '@/features/auth/api/getUser'
+import { User } from '@/features/auth/models/User'
+import { useEffect, useRef, useState } from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 
@@ -7,6 +10,24 @@ type MainLayoutProps = {
 
 /** メインレイアウト */
 export const MainLayout = ({ children }: MainLayoutProps) => {
+  /** ユーザー情報 */
+  const [user, setUser] = useState<User>()
+  /** ユーザー取得 */
+  const _getUser = async () => {
+    const response = await getUser()
+    setUser(response.data.user)
+  }
+  /** React.StrictMode対処用ref */
+  const didLogRef = useRef(false)
+  // マウント時実行イベント
+  useEffect(() => {
+    //  一度のみ実行
+    if (didLogRef.current === false) {
+      didLogRef.current = true
+      _getUser()
+    }
+  }, [])
+
   return (
     <>
       <Navbar bg="dark" variant="dark">
@@ -22,6 +43,9 @@ export const MainLayout = ({ children }: MainLayoutProps) => {
               Detail
             </Nav.Link>
           </Nav>
+          <Navbar.Text className="justify-content-end">
+            {user?.userName}
+          </Navbar.Text>
         </Container>
       </Navbar>
       <main>{children}</main>
--- src/test/dataGenerators/userGenerators.ts
+++ src/test/dataGenerators/userGenerators.ts
@@ -1,11 +1,11 @@
 import { User } from '@/features/auth/models/User'
-import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker/locale/ja'
 
 /** モック用ユーザーデータ生成 */
 export const userGenerator = (): User => {
   return {
     userId: faker.datatype.string(7),
-    userName: faker.name.lastName + ' ' + faker.name.firstName(),
+    userName: faker.name.lastName() + ' ' + faker.name.firstName(),
     email: faker.internet.email(),
   }
 }
